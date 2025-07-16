2aca855992a0a69331854208693237db0817657b
Nguyen Tran Chinh <chinh97@Mac-mini-cua-Nguyen.local>
2025-07-16 17:46:58 +0700

    add db

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  components: [
+    {
+      path: "./components",
+      pathPrefix: false,
+    },
+  ],
+  runtimeConfig: {
+    JWT_TOKEN_KEY: process.env.JWT_TOKEN_KEY,
+    REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  modules: ["@nuxtjs/tailwindcss", "@prisma/nuxt"],
+});
